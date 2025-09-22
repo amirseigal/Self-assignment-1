79ebd75ba1608957077363022a631a77ac441101
Sana-Mhd <sana2001mohammadi@gmail.com>
2025-09-22 16:01:54 +0330

    Add files via upload

---
+++ Task2/src/PrimeNumbers/PrimeNumbers.java
@@ -0,0 +1,24 @@
+package PrimeNumbers;
+
+public class PrimeNumbers {
+	
+	    public static void main(String[] args) {
+	        int nValues = 50; 
+	        System.out.println("Prime numbers up to " + nValues + ":");
+
+	        outer:
+	        for (int i = 2; i <= nValues; i++) {
+	            int limit = (int) Math.sqrt(i);
+
+	            for (int j = 2; j <= limit; j++) {
+	                if (i % j == 0) {
+	                    continue outer; 
+	                }
+	            }
+	            System.out.print(i + " ");
+	        }
+	        System.out.println(); 
+	    }
+	}
+
+
